5e2372f555436ee593cb5fc109dae3f62143c92e
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-03-29 15:38:07 +0530

    Create startup.sh

---
+++ resources/startup/startup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# scorpion - UserBot
+
+# Copyright (C) 2021 scorpion
+
+#
+
+# This file is a part of < https://github.com/loverboyXD/scorpion/ >
+
+# PLease read the GNU Affero General Public License in <https://www.github.com/loverboyXD/scorpion/blob/mainfucker/LICENSE/>.
+
+ 
+
+echo "
+
+            ┏━━┓┏━┓┏━┓┏━┓┏━┓┏━━┓┏━┓┏━┳┓
+
+            ┃━━┫┃┏┛┃┃┃┃╋┃┃╋┃┗┃┃┛┃┃┃┃┃┃┃
+
+            ┣━━┃┃┗┓┃┃┃┃┓┫┃┏┛┏┃┃┓┃┃┃┃┃┃┃
+
+            ┗━━┛┗━┛┗━┛┗┻┛┗┛░┗━━┛┗━┛┗┻━┛
+
+      Visit @Scorpion_official_Support for updates related to bot!!
+
+"
+
+ 
+
+python3 -m scorpion
